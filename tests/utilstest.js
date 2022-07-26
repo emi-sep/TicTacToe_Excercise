d518455fa038907d379ac8a0e134ade8d159b231
irene argollo <ireneirene892@gmail.com>
2022-07-26 09:36:16 -0400

    add -p example

--- tests/utilstest.js
+++ tests/utilstest.js
@@ -6,6 +6,18 @@ function chau() {
     console.log("chau");
 }
 
+function B(){
+    return "Variable B";
+}
+
+function C(a){
+    return "Variable B";
+}
+
+function D(){
+    return "Variable B";
+}
+
 function imprimir(datos) {
     console.log(datos);
 }
